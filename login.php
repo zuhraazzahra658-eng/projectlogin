6bba410050f9af9aaf990d7b685935ecce920ca5
zuhraazzahra658 <zuhraazzahra658@gmail.com>
2025-10-02 10:13:25 +0700

    mendesain halaman login

---
+++ login.php
@@ -0,0 +1,15 @@
+< IDOCTYPE html>
+<html >
+<head>
+‹title>Login</title>
+</head>
+< body>
+<h2>Form Login</h2>
+‹?php if (lempty(Serror)) echo "<p style='color: red; ' ›Serror</p›"; ?>
+< form method="post">
+Username: <input type="text" name="username" [equired›‹bI›‹b›
+Password: <input type="password" name="passwoId" [equired›‹bI›‹b›
+‹button type="submit">Login</button>
+</form>
+</body>
+</html>
